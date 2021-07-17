09f4dcf642aa15d7bce11333c6a4f5495fde9640
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-07-17 12:22:06 +0300

    new commit

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 3000,
+    PORT: 8080,
     DB_CONNECTION_STRING: 'mongodb+srv://admin:admin@cluster0.hgozd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     TOKEN_SECRET: 'this is very secure11',
     COOKIE_NAME: 'SESSION_TOKEN'
