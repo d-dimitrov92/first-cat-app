3be517823e625327a1dc67d0afeaa1b52e450601
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-07-17 14:07:16 +0300

    added styles and error notifications

--- models/Cat.js
+++ models/Cat.js
@@ -2,8 +2,8 @@
 
 
 const schema = new Schema({
-    name: { type: String, required: [true, 'Please enter Title'] },
-    description: { type: String, required: [true, 'Write description'], maxLength: [50, 'Description must be max 50 chars!'] },
+    name: { type: String, required: [true, 'Please enter name of the cat'] },
+    description: { type: String, required: [true, 'Write description'], minLength: [10, 'Description must be minimum 10 chars!'] },
     imageUrl: { type: String, required: [true, 'Add image please'] },
     author: { type: Schema.Types.ObjectId, ref: 'User' }
 });
