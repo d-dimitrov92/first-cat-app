72293c11c7d2612547c5a981f63616c9f1a8e2ee
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-07-17 17:02:05 +0300

    improved search input

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     const cats = await req.storage.getAllCats(req.query);
+
+    const ctx = {
+        search: req.query.search,
+        cats
+    }
     
-    res.render('home', { cats });
+    res.render('home', ctx);
 });
 
 router.get('/about', async (req, res) => {
